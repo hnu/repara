1702dbb0a2e6a753c7102bc88838d4585f9e316d
kabal <hector.e.navarro.u@gmail.com>
2018-07-01 19:40:18 -0400

    Basic project

---
+++ repara/__init__.py
@@ -0,0 +1,8 @@
+app = Flask(__name__, instance_relative_config=True)
+
+# Load the default configuration
+app.config.from_object('config.default')
+
+# Load the configuration from the instance folder
+app.config.from_pyfile('config.py')
+
